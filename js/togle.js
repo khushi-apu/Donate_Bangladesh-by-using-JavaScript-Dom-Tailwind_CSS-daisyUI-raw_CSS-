09539d55dfe3b078d734912a293a970af78a8310
khushi-apu <tabassumkhushi17@gmail.com>
2024-10-04 01:37:24 +0600

    two button toggling

---
+++ js/togle.js
@@ -0,0 +1,34 @@
+document.getElementById('donation-btn').addEventListener('click',()=>{
+    
+    console.log('ok done')
+
+    document.getElementById('history-btn').classList.add(
+        "bg-clifford",
+        "text-white",
+         "hover:bg-clifford"
+  )
+
+  document.getElementById('donation-btn').classList.remove(
+    "bg-clifford",
+    "text-white",
+     "hover:bg-clifford"
+)
+
+})
+document.getElementById('history-btn').addEventListener('click',()=>{
+    
+    console.log('ok done')
+
+    document.getElementById('donation-btn').classList.add(
+        "bg-clifford",
+        "text-white",
+         "hover:bg-clifford"
+  )
+
+  document.getElementById('history-btn').classList.remove(
+    "bg-clifford",
+    "text-white",
+     "hover:bg-clifford"
+)
+
+})
